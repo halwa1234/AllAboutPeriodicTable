f931a7b2c2f11b9304bc270942bdeac16287f34a
halwa1234 <42021870+halwa1234@users.noreply.github.com>
2021-05-26 18:41:29 +0530

    Create BT.java

---
+++ src/binarytree/BT.java
@@ -0,0 +1,18 @@
+package binarytree;
+
+public class BT {
+	Element head;
+	
+	BT(){
+		head=null;
+	}
+	
+	void create(Element falsehead, Element now) {
+		if (head==null) {
+			head=now;
+		}
+		else if(head.atomicNo>now.atomicNo) {
+			
+		}
+	}
+}
