2208d5792bd8745e2a97b74ea7182c17a4a933e8
halwa1234 <42021870+halwa1234@users.noreply.github.com>
2021-05-26 20:37:37 +0530

    Update BT.java

--- src/binarytree/BT.java
+++ src/binarytree/BT.java
@@ -7,12 +7,15 @@ public class BT {
 		head=null;
 	}
 	
-	void create(Element falsehead, Element now) {
-		if (head==null) {
-			head=now;
-		}
-		else if(head.atomicNo>now.atomicNo) {
-			
-		}
+	void createByAtomicNo(Element falsehead, Element now) {
+		if (head==null)	head=now;
+		else if(falsehead.atomicNo>now.atomicNo) createByAtomicNo(falsehead.right,now);
+		else createByAtomicNo(falsehead.left,now);
+	}
+	
+	void createByName(Element falsehead, Element now) {
+		if (head==null)	head=now;
+		else if(falsehead.name.compareTo(head.name)>0) createByAtomicNo(falsehead.right,now);
+		else createByAtomicNo(falsehead.left,now);
 	}
 }
