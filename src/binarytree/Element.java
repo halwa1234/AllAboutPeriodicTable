b9631cb87c8083054561a42087edf36ada2df251
halwa1234 <42021870+halwa1234@users.noreply.github.com>
2021-05-28 21:04:36 +0530

    Update Element.java

--- src/binarytree/Element.java
+++ src/binarytree/Element.java
@@ -7,6 +7,8 @@ package binarytree;
 
 
 public class Element {
+	FileIO elementlist=new FileIO();
+	
 	String name;
 	String chemicalSymbol;
 	String block;
@@ -17,6 +19,44 @@ public class Element {
 	
 	Element right;
 	Element left;
+	
+	Element(){
+		name="";
+		chemicalSymbol="";
+		block="";
+		group="";
+		
+		atomicMass=0;
+		atomicNo=0;
+		
+		right=null;
+		left=null;
+		
+	}
+	
+	Element(int i){
+		name=elementlist.ReadCellData(i+2, 0);
+		chemicalSymbol=elementlist.ReadCellData(i+2, 1);
+		block="";
+		group=elementlist.ReadCellData(i+2, 3);
+		
+		atomicMass=Integer.parseInt(elementlist.ReadCellData(i+2, 5));
+		atomicNo=Integer.parseInt(elementlist.ReadCellData(i+2, 2));
+		
+		right=null;
+		left=null;
+	}
+	
+	void display(){
+		System.out.println(name+ " ("+chemicalSymbol+")\n Group- "+group
+				+ "\n Atomic Number-"+ atomicNo+ "\n Atomic Mass-"+ atomicMass);
+	}
+	
+	
+	public static void main(String[] args) {
+		Element one=new Element(1);
+		one.display();
+	}
 }
 
 
