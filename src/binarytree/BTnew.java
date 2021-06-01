2b9e3fe310d37f50356eecb16413af84ace24e5e
halwa1234 <42021870+halwa1234@users.noreply.github.com>
2021-06-01 17:34:40 +0530

    Update BTnew.java

--- src/binarytree/BTnew.java
+++ src/binarytree/BTnew.java
@@ -16,22 +16,19 @@ public class BTnew {
 	void automateNumber() {
 		int i=1;
 		Element n;
-		while(i<20) {
+		while(i<10) {
 			n=new Element(i);
 			createByAtomicNo(head, n);
-			System.out.println(i);
 			i++;
 		}
 		System.out.println("Created!!");
-		head.display();
 	}
 	void automateName() {
 		int i=1;
 		Element n;
-		while(i<113) {
+		while(i<10) {
 			n=new Element(i);
 			createByName(head, n);
-			System.out.println(i);
 			i++;
 		}
 		System.out.println("Created!!");
@@ -51,11 +48,11 @@ public class BTnew {
 		if (head==null)	head=now;
 		else if(falsehead.name.compareTo(head.name)>0) {
 			if (falsehead.right==null) falsehead.right=now;
-			else createByAtomicNo(falsehead.right,now);
+			else createByName(falsehead.right,now);
 		}
 		else {
 			if (falsehead.left==null) falsehead.left=now;
-			else createByAtomicNo(falsehead.left,now);
+			else createByName(falsehead.left,now);
 		}
 	}
 	//search by atomic number
@@ -64,6 +61,45 @@ public class BTnew {
 	
 	
 //################# SEARCHING ELEMENTS ######################	
+	public int atnumSearch(Element n,int d) {
+		  if(n.atomicNo==d)
+			  return n.atomicNo;
+		  if(n.atomicNo<d)
+			  if (n.right!=null) return atnumSearch(n.right,d);
+		  if (n.left!=null) return atnumSearch(n.left,d);
+		  return 0;
+	 }
+	
+	public int nameSearch(Element n,String d) {
+		if(n.name.compareTo(d)==0)
+			return n.atomicNo;
+		if(n.name.compareTo(d)>0) {
+			if (n.right!=null) return nameSearch(n.right,d);
+		}
+		if (n.left!=null) return nameSearch(n.left,d);
+		return 2;
+	 }
+	
+	public int atmassSearch(Element n,float d) {
+		  if(n==null || n.atomicMass==d)
+			  return n.atomicNo;
+		  if(n.atomicMass<d)
+			  return atmassSearch(n.right,d);
+		  return atmassSearch(n.left,d);
+	}
+	
+	
+	
+	
+	public int chemicalSymbolSearch(Element r,String n) {
+		  if(r==null || r.chemicalSymbol.compareTo(n)==0)
+			  return r.atomicNo;
+		  if(r.chemicalSymbol.compareTo(n)<0)
+			  return nameSearch(r.right,n);
+		  return nameSearch(r.left,n);
+	}
+//################# DISPLAYING ######################	
+	
 	void display(Element falsehead){
 		if (falsehead.left!=null) display(falsehead.left);
 		falsehead.display();
@@ -91,9 +127,7 @@ public class BTnew {
 			if (falsehead.atomicNo==i) falsehead.display();
 			if (falsehead.right!=null) displayElement(i,falsehead.right);
 	}
-		  
-//################# DISPLAYING ######################	
-	  
+		 
 	
 	
 	
@@ -102,22 +136,26 @@ public class BTnew {
 	public static void main (String[] args) {
 		FileIO special=new FileIO();
 		
-		/*BTnew tree=new BTnew();
+		BTnew tree=new BTnew();
 		System.out.println("Automating");
 
-		tree.automateNumber();
+		tree.automateName();
 		System.out.println("Displaying");
 
 		tree.display(tree.gethead());
 		System.out.println("\nDone.\n");
-		
+	/*	
 		tree.displayGroup("Non-Metal",tree.gethead());
 		System.out.println("\nDone. Starting next\n");
 
 		tree.displayBlock("s",tree.gethead());
 		
 		*/
-		special.displaySpecial(6);
+		//special.displaySpecial(tree.atnumSearch(tree.gethead(), 3));
+		special.displaySpecial(tree.nameSearch(tree.gethead(),"Lithium" ));
+
+		//special.displaySpecial(5);
+
 	}
 
 }
