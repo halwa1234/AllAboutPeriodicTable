885c3b06c5289defdd2b9c58549296b040546135
pallavi-s7 <84673491+pallavi-s7@users.noreply.github.com>
2021-06-01 18:03:03 +0530

    Update Main.java

--- src/binarytree/Main.java
+++ src/binarytree/Main.java
@@ -8,29 +8,54 @@ public class Main {
 	
 	Scanner sc=new Scanner(System.in);
 	String s;
-	int ch=1;
+	int ch=1,n,n1;
+	float m;
 	while(ch!=0){
 		System.out.println("**********MENU**************");
 		System.out.println(" 1. View all elements \n 2. View elements by block \n 3. View elements by group \n 4. View information of particular element \n 5. Play Quiz \n 0.Exit");
 		System.out.println("Enter your choice : ");
 		ch=sc.nextInt();
-		
+		int sch=0;
 		switch(ch){
 			case 0:System.out.println("\n\nExiting...");
 					System.exit(0);
 			case 1:System.out.println("All elements of Periodic table are:");
 				element.display(element.head);
 				break;
-			case 2:System.out.println("Enter block of elements to be displayed:");
-				s=sc.next();
+			case 2:System.out.println("Enter block of elements to be displayed 's,p,d,f':");
+				s=sc.nextLine();
 				element.displayBlock(s,element.head);
 				break;
 		
 			case  3 :   System.out.println("Enter the group name\n 0.Exit \n 1.Alkali metal \n 2.Alkali Earth Metal \n 3.Transition Metal \n 4.Metal \n 5.Non-metal \n 6.Noble Gas \n 7.Halogen  \n 8.Rare Earth : ");
-				   s=sc.next();
+				   s=sc.nextLine();
 				   element.displayGroup(s,element.head);
 				   break;
-			case 4://element.search();
+			case 4:System.out.println("Enter search choice\n1Atomic number \n2.Atomic mass number \n3.Name \n4.Symbol");
+				sch=sc.nextInt();
+				switch(sch){
+					case 1:System.out.println("Enter atomic number");
+						n=sc.nextInt();
+						n1=atnumSearch(element.head,n);
+						displayElement(n1,element.head);
+						break;
+					case 2:System.out.println("Enter atomic mass number");
+						m=sc.nextFloat();
+						n1=atmassSearch(element.head,m);
+						displayElement(n1,element.head);
+						break;
+					case 3:System.out.println("Enter name");
+						s=sc.nextLine();
+						n1=nameSearch(element.head,s);
+						displayElement(n1,element.head);
+						break;
+					case 4:Syatem.out.println("Enter symbol");
+						s=sc.nextLine();
+						n1=chemicalSymbolSearch(element.head,s);
+						displayElement(n1,element.head);
+						break;
+					default:System.out.println("Enter correct choice");
+				}
 				break;
 			case 5://quiz
 				break;
